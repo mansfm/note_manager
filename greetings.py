b979f259d1587468bae5aa060df75664a6918782
mansfm <91097878+mansfm@users.noreply.github.com>
2025-01-07 02:56:51 +0300

    Update greetings.py

--- greetings.py
+++ greetings.py
@@ -1,1 +1,13 @@
+username = "Тестовое имя"
+title = "Тестовый заголовок"
+content = "Тестовое описание"
+status = "Тестовый статус"
+created_date = "10-11-2024"
+issue_date = "10-12-2024"
 
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", created_date)
+print("Дата истечения заметки (дедлайн):", issue_date)
