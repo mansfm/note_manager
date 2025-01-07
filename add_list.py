40052c35c891520234f558890f5228f991ce0891
mansfm <91097878+mansfm@users.noreply.github.com>
2025-01-07 05:43:43 +0300

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,7 @@
+title1 = input("Введите первый заголовок заметки: ")
+title2 = input("Введите второй заголовок заметки: ")
+title3 = input("Введите третий заголовок заметки: ")
+
+title_list = [title1, title2, title3]
+
+print(title_list)
